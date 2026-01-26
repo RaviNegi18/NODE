69706ecffdb3711a7d6d6b2b54d4a15da2d77382
Ravinder singh <negiravi451@gmail.com>
2026-01-26 17:36:57 +0530

    completed

---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import React from "react";
+const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("Submitting...", formData);
+
+    try {
+      // ---- Example: Send Data to Backend ----
+      const res = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+        // credentials: "include", // for cookiess
+      });
+
+      const data = await res.json();
+      console.log("Response:", data);
+
+      // Reset form
+      setFormData({
+
+        email: "",
+        password: "",
+      });
+
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  return (
+    <div className="flex items-center justify-center flex-col mt-10 gap-4">
+      <h1 className="text-2xl font-bold">Login  Here</h1>
+
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-64">
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+          className="p-2 bg-slate-500 text-white rounded"
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          className="p-2 bg-slate-500 text-white rounded"
+        />
+
+        <button
+          type="submit"
+          className="bg-blue-600 text-white py-2 rounded hover:bg-blue-700"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
