5e214ea78ff3d20701e1e6bbf8978a13e47e7d98
Ravinder singh <negiravi451@gmail.com>
2026-02-05 22:47:28 +0530

    refressh acces Token in routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express"
 import { userRegister } from "../controllers/user.controller.js";
 import { userLogin } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middleware/authmiddleware.js";
-
+import { refreshAccessToken } from "../controllers/refreshToken-controller.js";
 const router=express.Router()
 
 router.post("/register",authMiddleware, userRegister)
